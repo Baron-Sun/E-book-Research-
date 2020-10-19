5ec96506872391f3aa407a2b7ec863a9247bf6b9
Kanglan Tang <karenkanglantang@gmail.com>
2020-10-18 22:19:47 -0700

    build and removed node modules

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.efd40cdec963c030d2be24c259900b69.js"
+  "/precache-manifest.bd0c328fa6e258c3c4ed0bd8061b71b1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
